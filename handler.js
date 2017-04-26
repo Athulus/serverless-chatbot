b6437a9aad31338be7163422f1d46223ca46b52c
Max Redmond <athulus1991@gmail.com>
2017-04-26 11:56:27 -0400

    lesson 2 complete

--- handler.js
+++ handler.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const https = require('https');
-const qs = require('querystring')
+const qs = require('querystring');
 
-const direct_mention = new RegExp('^\<@' + process.env.BOT_ID + '\>','i')
+const direct_mention = new RegExp('^\<\@' + process.env.BOT_ID + '\>', 'i');
 
 module.exports.endpoint = (event, context, callback) => {
+  console.log('Received event', event);
+
   const request = JSON.parse(event.body);
 
   if (request.type === 'url_verification' && request.token === process.env.VERIFICATION_TOKEN) {
@@ -21,31 +23,27 @@ module.exports.endpoint = (event, context, callback) => {
       return callback(null, response);
   }
 
-  if (request.event.text.match(direct_mention)){
+  if (request.event.text.match(direct_mention)) {
     console.log('Echoing message back to slack');
+
     const response = {
-      token: process.env.BOT_ID,
+      token: process.env.BOT_ACCESS_TOKEN,
       channel: request.event.channel,
-      text: request.event.text.replace(direct_mention,'') + ' [' +Date.now() +']'
+      text: request.event.text.replace(direct_mention, '') + ' [' + Date.now() + ']'
     }
+
+    const URL = process.env.POST_MESSAGE_URL + qs.stringify(response);
+
+    https.get(URL, (res) => {
+      const statusCode = res.statusCode;
+
+      console.log(direct_mention, request.event.text.replace(direct_mention, ''), statusCode);
+
+      if (statusCode !== 200) {
+        console.log(res);
+      }
+
+      callback(null, {statusCode: 200});
+    })
   }
-
-  const URL = process.env.POST_MESSAGE_URL + qs.stringify(response);
-
-  https.get(URL, (res) => {
-    const statusCode = res.statusCode;
-
-    console.log(direct_mention, request.event.text.replace(direct_mention, ''), statusCode);
-
-    if (statusCode !== 200) {
-      console.log(res);
-    }
-
-    callback(null, {statusCode: 200});
-  })
-  };
-
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
